cecfe84db7ff5fbf0a018a8ec792654e8cc99d5d
srinivasjsutar <sjsjasss2001@gmail.com>
2026-09-01 15:13:22 +0530

    updated the backend server

--- src/server.js
+++ src/server.js
@@ -18,8 +18,8 @@ const marketRoutes      = require('./routes/market')
 const voiceflowRoutes   = require('./routes/voiceflow')
 const adminRoutes       = require('./routes/admin')
 const partnerRoutes     = require('./routes/partners')
-// const paymentRoutes = require('./routes/payments') // uncomment after adding Razorpay keys
-// const salesRoutes   = require('./routes/sales')     // push sales.js to repo first
+// const paymentRoutes = require('./routes/payments') // push payments.js to repo first
+const salesRoutes   = require('./routes/sales')     // push sales.js to repo first
 
 // Connect to MongoDB
 connectDB()
@@ -81,8 +81,8 @@ app.use('/api/blogs',        blogRoutes)
 app.use('/api/faqs',         faqRoutes)
 app.use('/api/applications', applicationRoutes)
 app.use('/api/market',       marketRoutes)
-// app.use('/api/payments', paymentRoutes) // uncomment after adding Razorpay keys
-// app.use('/api/sales',    salesRoutes)   // uncomment after pushing sales.js to repo
+// app.use('/api/payments', paymentRoutes)
+app.use('/api/sales',    salesRoutes)
 app.use('/api/voiceflow',    voiceflowRoutes)
 app.use('/api/admin',        adminRoutes)
 app.use('/api/partners',     partnerRoutes)
